43d9e060372e215fe0ef463ab743d4460d2e0b70
mraab <mraab@intershop.de>
2016-04-10 11:21:30 +0200

    adapt build configuration

--- buildTravis.sh
+++ buildTravis.sh
@@ -4,17 +4,17 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     if [ "$TRAVIS_TAG" == "" ]; then
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-        ./gradlew test build bintrayUpload
+        ./gradlew test build
     else
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH'] and without Tag'
-        ./gradlew test build
+        ./gradlew test build bintrayUpload
     fi
 else
     if [ "$TRAVIS_TAG" == "" ]; then
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH']  Tag ['$TRAVIS_TAG']'
-        ./gradlew test build bintrayUpload
+        ./gradlew test build
     else
         echo -e 'Build Branch for Release => Branch ['$TRAVIS_BRANCH'] and without Tag'
-        ./gradlew test build
+        ./gradlew test build bintrayUpload
     fi
 fi
