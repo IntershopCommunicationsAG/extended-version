03de9425c13611e1bdd8c842ed7103f339c5c11a
mraab <mraab@intershop.de>
2016-04-10 11:09:05 +0200

    adapt documentation and build configuration

--- src/main/groovy/com/intershop/release/version/MetadataVersion.groovy
+++ src/main/groovy/com/intershop/release/version/MetadataVersion.groovy
@@ -12,6 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  *  limitations under the License.
+ *
+ * This file includes code fragments from semver (https://github.com/zafarkhaja/jsemver),
+ * which is licensed under the MIT license.
  */
 package com.intershop.release.version
 
